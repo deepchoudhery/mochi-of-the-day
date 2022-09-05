ce122366b7e418e1fc0bda05c4d96553f5feb0d5
deepchoudhery <decho@microsoft.com>
2022-09-05 13:40:11 -0700

    changed project name, added Word model.

---
+++ Word.cs
@@ -0,0 +1,10 @@
+﻿namespace MochiOfTheDay
+{
+    public class Word
+    {
+        public int Id { get; set; }
+        public string? WordOfTheDay { get; set; }
+        public string? Definition { get; set; }
+        public DateOnly Date { get; set; }
+    }
+}
